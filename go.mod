177ce3c77f13ebe74f297d8883a6be66f9f1c51b
Anurag Patil <anurag.patil@headout.com>
2021-05-23 00:01:55 +0530

    inital working POC

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/meta-boy/dolus
 
 go 1.16
+
+require github.com/atotto/clipboard v0.1.4 // indirect
--- main.go
+++ main.go
@@ -1,25 +1,86 @@
 package main
 
 import (
-    "flag"
-    "fmt"
+	"bytes"
+	"encoding/json"
+	"flag"
+	"github.com/atotto/clipboard"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"time"
 )
+
+type author struct {
+	Name string `json:"name"`
+}
+
+type embed struct {
+	Description string `json:"description"`
+	Author      author `json:"author"`
+}
+
+type embeds struct {
+	Embeds []embed `json:"embeds"`
+}
+
+func send(hook string, content string, user string) {
+	embeds := embeds{
+		Embeds: []embed{
+			{
+				Description: content,
+				Author: author{
+					Name: user,
+				},
+			},
+		},
+	}
+
+	postBody, _ := json.Marshal(embeds)
+
+	responseBody := bytes.NewBuffer(postBody)
+
+	resp, err := http.Post(hook, "application/json", responseBody)
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	body, err := ioutil.ReadAll(resp.Body)
+
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	sb := string(body)
+	log.Println(sb)
+
+}
 
 func main() {
 
-    wordPtr := flag.String("word", "foo", "a string")
+	webhookPtr := flag.String("webhook", "foo", "Discord webhook")
+	userPtr := flag.String("user", "Anon", "an int")
 
-    numbPtr := flag.Int("numb", 42, "an int")
-    boolPtr := flag.Bool("fork", false, "a bool")
+	flag.Parse()
 
-    var svar string
-    flag.StringVar(&svar, "svar", "bar", "a string var")
+	oldContent, err := clipboard.ReadAll()
 
-    flag.Parse()
+	if err != nil {
+		log.Fatalln(err)
+	}
 
-    fmt.Println("word:", *wordPtr)
-    fmt.Println("numb:", *numbPtr)
-    fmt.Println("fork:", *boolPtr)
-    fmt.Println("svar:", svar)
-    fmt.Println("tail:", flag.Args())
+	for {
+		content, err := clipboard.ReadAll()
+		if err != nil {
+			log.Fatalln(err)
+		}
+
+		if oldContent != content {
+			send(*webhookPtr, content, *userPtr)
+			log.Println("Send content")
+			oldContent = content
+
+		}
+		time.Sleep(500 * time.Millisecond)
+	}
 }
